1c8cc92de1f5b37b854aa925f1a740bc6b9f68a8
Robert Fiser <robert.fiser@socialbakers.com>
2015-02-24 13:02:20 +0100

    dynamic index and type name + ignoreFields

--- src/main/java/com/sematext/morphline/elasticsearch/LoadElasticsearchBuilder.java
+++ src/main/java/com/sematext/morphline/elasticsearch/LoadElasticsearchBuilder.java
@@ -18,15 +18,14 @@ package com.sematext.morphline.elasticsearch;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
-import com.codahale.metrics.Timer;
-import com.google.common.annotations.VisibleForTesting;
-import com.typesafe.config.Config;
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.kitesdk.morphline.api.Command;
@@ -35,8 +34,13 @@ import org.kitesdk.morphline.api.MorphlineContext;
 import org.kitesdk.morphline.api.MorphlineRuntimeException;
 import org.kitesdk.morphline.api.Record;
 import org.kitesdk.morphline.base.AbstractCommand;
+import org.kitesdk.morphline.base.FieldExpression;
 import org.kitesdk.morphline.base.Metrics;
 import org.kitesdk.morphline.base.Notifications;
+
+import com.codahale.metrics.Timer;
+import com.google.common.annotations.VisibleForTesting;
+import com.typesafe.config.Config;
 
 /**
  * A command that loads a record into an Elasticsearch server.
@@ -48,6 +52,7 @@ public class LoadElasticsearchBuilder implements CommandBuilder {
   public static final String TTL = "ttl";
   public static final String INDEX_NAME = "index";
   public static final String TYPE = "type";
+  public static final String IGNORE_FIELDS = "ignoreFields";
 
   @Override
   public Collection<String> getNames() {
@@ -65,8 +70,9 @@ public class LoadElasticsearchBuilder implements CommandBuilder {
   private static final class LoadElasticsearch extends AbstractCommand {
     private DocumentLoader loader;
     private final Timer elapsedTime;
-    private final String indexName;
-    private final String indexType;
+    private final FieldExpression indexName;
+    private final FieldExpression indexType;
+    private final Set<String> ignoreFields;
     private final int ttl;
 
     LoadElasticsearch(CommandBuilder builder, Config config, Command parent, Command child, MorphlineContext context) {
@@ -74,8 +80,9 @@ public class LoadElasticsearchBuilder implements CommandBuilder {
       elapsedTime = getTimer(Metrics.ELAPSED_TIME);
       Config elasticsearchConfig = getConfigs().getConfig(config, ELASTICSEARCH_CONFIGURATION);
       String loaderType = getConfigs().getString(config, DOCUMENT_LOADER_TYPE);
-      indexName = getConfigs().getString(config, INDEX_NAME);
-      indexType = getConfigs().getString(config, TYPE);
+      indexName = new FieldExpression(getConfigs().getString(config, INDEX_NAME), getConfig());
+      indexType = new FieldExpression(getConfigs().getString(config, TYPE), getConfig());
+      ignoreFields = new LinkedHashSet<String>(getConfigs().getStringList(config, IGNORE_FIELDS, new ArrayList<String>()));
       ttl = getConfigs().getInt(config, TTL);
       validateArguments();
 
@@ -133,6 +140,9 @@ public class LoadElasticsearchBuilder implements CommandBuilder {
         Map<String, Collection<Object>> map = record.getFields().asMap();
         for (Map.Entry<String, Collection<Object>> entry : map.entrySet()) {
           String key = entry.getKey();
+          if (ignoreFields.contains(key)) {
+            continue;
+          }
           Collection<Object> values = entry.getValue();
           Iterator<Object> iterator = values.iterator();
           if (values.size() > 1) {
@@ -146,7 +156,9 @@ public class LoadElasticsearchBuilder implements CommandBuilder {
           }
         }
         documentBuilder.endObject();
-        loader.addDocument(documentBuilder.bytes(), indexName, indexType, ttl);
+        String name = indexName.evaluate(record).get(0).toString();
+        String type = indexType.evaluate(record).get(0).toString();
+        loader.addDocument(documentBuilder.bytes(), name, type, ttl);
       } catch (Exception e) {
         throw new MorphlineRuntimeException(e);
       } finally {
